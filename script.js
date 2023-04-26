3233d41252b392d5ac0f2e020c5ce01382414f34
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-26 11:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function insert_Row() {
 		<tr><td>New cell1</td> 
 		<td>New cell2</td></tr>
 		"
-  
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
   
 }
