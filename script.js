86de9d0568c85006ef0a631439b92a9678fb6451
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-26 11:28:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
+	let table = document.getElementBuId("sampleTable");
+	table.innerHTML = "
+		<tr><td>New cell1</td> 
+		<td>New cell2</td></tr>
+		"
   
   
 }
